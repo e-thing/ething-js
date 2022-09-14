5bc0f2b84c6d06ddc8092afe52fa12b61feee409
amezerette <a.mezerette@gmail.com>
2022-09-14 18:18:26 +0200

    0.1.26

--- src/ething.js
+++ src/ething.js
@@ -6,7 +6,7 @@
 var EThing = require("./core.js");
 
 // do not touch the following line, the version number will be automatically updated when executing 'npm version'
-EThing.VERSION = '0.1.25';
+EThing.VERSION = '0.1.26';
 
 require("./resource.js");
 require("./settings.js");
